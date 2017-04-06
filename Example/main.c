1d321cd49a39958554ec3c209af35d05fb59670e
YourtionGuo <yourtion@gmail.com>
2017-04-06 18:13:23 +0800

    init project

---
+++ Example/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  Example
+//
+//  Created by YourtionGuo on 05/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
