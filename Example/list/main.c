6c7c22088ec4a97b8a7e1a0c5ac13979b0919cd0
YourtionGuo <yourtion@gmail.com>
2017-04-18 15:28:58 +0800

    add C05-List Example

---
+++ Example/list/main.c
@@ -0,0 +1,16 @@
+//
+//  main.c
+//  MasteringAlgorithms
+//
+//  Created by YourtionGuo on 18/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+  // insert code here...
+  printf("Hello, World! C05\n");
+  return 0;
+}
+
