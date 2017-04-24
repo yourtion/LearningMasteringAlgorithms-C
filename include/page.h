1fc42fc9c9b7be3597aa58e12b18bdcd2f46985f
YourtionGuo <yourtion@gmail.com>
2017-04-24 17:27:28 +0800

    add page.h (5-9)

---
+++ include/page.h
@@ -0,0 +1,33 @@
+//
+//  page.h
+//  Algorithms
+//
+//  Created by YourtionGuo on 24/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#ifndef PAGE_H
+#define PAGE_H
+
+#include "clist.h"
+
+/**
+ 页
+ */
+typedef struct Page_
+{
+  int   number;
+  int   reference;
+  
+} Page;
+
+
+/**
+ 第二次机会置换法（时钟算法）LRU
+
+ @param current 当前元素
+ @return 页位置
+ */
+int replace_page(CListElmt **current);
+
+#endif /* PAGE_H */
