48260d4b3e807afc23e3e353c8114a980c493de5
YourtionGuo <yourtion@gmail.com>
2017-04-24 17:27:15 +0800

    add page (5-8)

---
+++ source/page.c
@@ -0,0 +1,26 @@
+//
+//  page.c
+//  Algorithms
+//
+//  Created by YourtionGuo on 24/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include "clist.h"
+#include "page.h"
+
+int replace_page(CListElmt **current)
+{
+  
+  /// 循环整个链表直至找到结果
+  
+  while (((Page *)(*current)->data)->reference != 0) {
+    
+    ((Page *)(*current)->data)->reference = 0;
+    *current = clist_next(*current);
+    
+  }
+  
+  return ((Page *)(*current)->data)->number;
+  
+}
