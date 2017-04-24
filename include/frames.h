0a8fa7ab2a208b30201903ae354763ab5f0f812e
YourtionGuo <yourtion@gmail.com>
2017-04-24 14:43:52 +0800

    add frames (5-3)

---
+++ include/frames.h
@@ -0,0 +1,34 @@
+//
+//  frames.h
+//  Algorithms
+//
+//  Created by YourtionGuo on 24/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#ifndef FRAMES_H
+#define FRAMES_H
+
+#include "list.h"
+
+
+/**
+ 从空闲页帧链表中获取空闲页帧号
+
+ @param frames 帧链表
+ @return 页帧号
+ */
+int alloc_frame(List *frames);
+
+
+
+/**
+ 释放之前获取的页帧号
+
+ @param frames 帧链表
+ @param frame_number 页帧号
+ @return 成功返回 0，否则返回 -1
+ */
+int free_frame(List *frames, int frame_number);
+
+#endif /* FRAMES_H */
---
+++ source/frames.c
@@ -0,0 +1,48 @@
+//
+//  frames.c
+//  Algorithms
+//
+//  Created by YourtionGuo on 24/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include <stdio.h>
+#include "frames.h"
+#include "list.h"
+
+int alloc_frame(List *frames)
+{
+  int frame_number, *data;
+  if (list_size(frames) == 0) {
+    /// 如果 frames 没有空间，返回错误
+    return -1;
+  } else {
+    if (list_rem_next(frames, NULL, (void **)&data) != 0) {
+      /// 如果找不到空闲的 frame，返回错误
+      return -1;
+    } else {
+      /// 从 frames 中获取一个空闲的帧号
+      frame_number = *data;
+      free(data);
+    }
+  }
+  return frame_number;
+}
+
+int free_frame(List *frames, int frame_number)
+{
+  int *data;
+  
+  /// 获取存储页帧号的空间
+  if ((data = (int *) malloc(sizeof(int))) == NULL) {
+    return -1;
+  }
+  
+  /// 将对应的页帧放回帧链表
+  *data = frame_number;
+  if (list_ins_next(frames, NULL, data) != 0) {
+    return -1;
+  }
+  
+  return 0;
+}
