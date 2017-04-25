5f8d2f91f347db16f334d0994c1117ab576e29f2
YourtionGuo <yourtion@gmail.com>
2017-04-25 17:19:02 +0800

    add queue (6-4)

---
+++ source/queue.c
@@ -0,0 +1,34 @@
+//
+//  queue.c
+//  Algorithms - queue
+//
+//  Created by YourtionGuo on 25/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include "list.h"
+#include "queue.h"
+
+/*****************************************************************************
+ *                                                                            *
+ *  ----------------------------- queue_enqueue ----------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int queue_enqueue(Queue *queue, const void *data)
+{
+  /// 入队
+  return list_ins_next(queue, list_tail(queue), data);
+}
+
+/*****************************************************************************
+ *                                                                            *
+ *  ----------------------------- queue_dequeue ----------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int queue_dequeue(Queue *queue, void **data)
+{
+  /// 出队
+  return list_rem_next(queue, NULL, data);
+}
