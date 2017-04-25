14557458a48122c1f853a60db3e56ef0a270827e
YourtionGuo <yourtion@gmail.com>
2017-04-25 17:31:34 +0800

    add events (6-5)

---
+++ include/events.h
@@ -0,0 +1,43 @@
+//
+//  events.h
+//  Algorithms - events
+//
+//  Created by YourtionGuo on 25/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#ifndef EVENTS_H
+#define EVENTS_H
+
+#include "queue.h"
+
+/**
+ 事件
+ */
+typedef struct Event_
+{
+  int   type;
+  
+} Event;
+
+
+/**
+ 将要处理的事件入队 - O(1)
+
+ @param events 事件队列
+ @param event 事件
+ @return 成功返回 0，否则返回 -1
+ */
+int receive_event(Queue *events, const Event *event);
+
+
+/**
+ 事件从队列中出队 - O(1)
+
+ @param events 事件队列
+ @param dispatch 调度函数
+ @return 成功返回 0，否则返回 -1
+ */
+int process_event(Queue *events, int (*dispatch)(Event *event));
+
+#endif /* events_h */
---
+++ source/events.c
@@ -0,0 +1,56 @@
+//
+//  events.c
+//  Algorithms - events
+//
+//  Created by YourtionGuo on 25/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include <stdio.h>
+#include <string.h>
+
+#include "events.h"
+#include "queue.h"
+
+/*****************************************************************************
+ *                                                                            *
+ *  ---------------------------- receive_event -----------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int receive_event(Queue *events, const Event *event)
+{
+  Event   *new_event;
+  
+  /// 创建事件对应存储空间
+  if ((new_event = (Event *)malloc(sizeof(Event))) == NULL) return -1;
+  
+  /// 拷贝事件并入列
+  memcpy(new_event, event, sizeof(Event));
+  
+  if (queue_enqueue(events, new_event) != 0) return -1;
+  
+  return 0;
+}
+
+/*****************************************************************************
+ *                                                                            *
+ *  ---------------------------- process_event -----------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int process_event(Queue *events, int (*dispatch)(Event *event))
+{
+  Event   *event;
+  
+  /// 如果没有事件待处理，返回 -1
+  if (queue_size(events) == 0) return -1;
+  
+  /// 如果无法获取事件，返回 -1
+  if (queue_dequeue(events, (void **)&event) != 0) return -1;
+  
+  dispatch(event);
+  free(event);
+
+  return 0;
+}
