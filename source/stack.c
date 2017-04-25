76615336661abf258541d4d37ba8a60ab3c250a9
YourtionGuo <yourtion@gmail.com>
2017-04-25 16:44:15 +0800

    add stack (6-2)

---
+++ source/stack.c
@@ -0,0 +1,38 @@
+//
+//  stack.c
+//  Algorithms - stack
+//
+//  Created by YourtionGuo on 25/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include <stdio.h>
+
+#include "list.h"
+#include "stack.h"
+
+
+/*****************************************************************************
+ *                                                                            *
+ *  ------------------------------ stack_push ------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int stack_push(Stack *stack, const void *data)
+{
+  /// 将数据压入栈
+  return list_ins_next(stack, NULL, data);
+}
+
+
+/*****************************************************************************
+ *                                                                            *
+ *  ------------------------------ stack_pop -------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int stack_pop(Stack *stack, void **data)
+{
+  /// 弹出栈顶元素
+  return list_rem_next(stack, NULL, data);
+}
