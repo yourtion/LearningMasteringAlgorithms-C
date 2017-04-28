7e6a514ed0aa11c06b967eb735d7f5a6a7a4bb5b
YourtionGuo <yourtion@gmail.com>
2017-04-28 15:04:04 +0800

    add hashpjw (8-1)

---
+++ include/hashpjw.h
@@ -0,0 +1,30 @@
+//
+//  hashpjw.h
+//  Algorithms
+//
+//  Created by YourtionGuo on 28/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+/*****************************************************************************
+ *                                                                            *
+ *  ------------------------------- hashpjw.h ------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+#ifndef HASHPJW_H
+#define HASHPJW_H
+
+/// 定义桶数量（表大小）
+#define     PRIME_TBLSIZ       1699
+
+
+/**
+ 处理字符串的哈希函数
+
+ @param key 待处理键
+ @return 哈希
+ */
+int hashpjw(const void *key);
+
+#endif /* HASHPJW_H */
---
+++ source/hashpjw.c
@@ -0,0 +1,44 @@
+//
+//  hashpjw.c
+//  Algorithms
+//
+//  Created by YourtionGuo on 28/04/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include "hashpjw.h"
+
+/*****************************************************************************
+ *                                                                            *
+ *  -------------------------------- hashpjw -------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int hashpjw(const void *key)
+{
+  const char    *ptr;
+  int           val;
+  
+  /// 通过一系列位操作对提供的 key 进行 hash
+  
+  val = 0;
+  ptr = key;
+  
+  while (*ptr != '\0') {
+    
+    int tmp;
+    val = (val << 4) + (*ptr);
+    
+    if ((tmp = (val & 0xf0000000))) {
+      
+      val = val ^ (tmp >> 24);
+      val = val ^ tmp;
+      
+    }
+    
+    ptr++;
+  }
+
+  /// 根据实际情况将 PRIME_TBLSIZ 替换为需要的表大小
+  return val % PRIME_TBLSIZ;
+}
