ed5f9f2e4b5900822b402c830c7b06d655126841
YourtionGuo <yourtion@gmail.com>
2017-05-03 15:32:20 +0800

    add traverse (9-3)

---
+++ source/traverse.c
@@ -0,0 +1,85 @@
+//
+//  traverse.c
+//  Algorithms - bitree traverse
+//
+//  Created by YourtionGuo on 03/05/2017.
+//  Copyright © 2017 Yourtion. All rights reserved.
+//
+
+#include "list.h"
+#include "traverse.h"
+
+/*****************************************************************************
+ *                                                                            *
+ *  ------------------------------- preorder -------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int bitree_preorder(const BiTreeNode *node, List *list)
+{
+  if (!bitree_is_eob(node)) {
+    
+    if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0) return -1;
+    
+    if (!bitree_is_eob(bitree_left(node))) {
+      if (bitree_preorder(bitree_left(node), list) != 0) return -1;
+    }
+    
+    if (!bitree_is_eob(bitree_right(node))) {
+      if (bitree_preorder(bitree_right(node), list) != 0) return -1;
+    }
+    
+  }
+  
+  return 0;
+}
+
+/*****************************************************************************
+ *                                                                            *
+ *  -------------------------------- inorder -------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int bitree_inorder(const BiTreeNode *node, List *list)
+{
+  if (!bitree_is_eob(node)) {
+    
+    if (!bitree_is_eob(bitree_left(node))) {
+      if (bitree_inorder(bitree_left(node), list) != 0) return -1;
+    }
+    
+    if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0) return -1;
+    
+    if (!bitree_is_eob(bitree_right(node))) {
+      if (bitree_inorder(bitree_right(node), list) != 0) return -1;
+    }
+    
+  }
+  
+  return 0;
+}
+
+/*****************************************************************************
+ *                                                                            *
+ *  ------------------------------- postorder ------------------------------  *
+ *                                                                            *
+ *****************************************************************************/
+
+int bitree_postorder(const BiTreeNode *node, List *list)
+{
+  if (!bitree_is_eob(node)) {
+    
+    if (!bitree_is_eob(bitree_left(node))) {
+      if (bitree_postorder(bitree_left(node), list) != 0) return -1;
+    }
+    
+    if (!bitree_is_eob(bitree_right(node))) {
+      if (bitree_postorder(bitree_right(node), list) != 0) return -1;
+    }
+    
+    if (list_ins_next(list, list_tail(list), bitree_data(node)) != 0) return -1;
+    
+  }
+  
+  return 0;
+}
