53e58f2d044d87ea95af382273396b9c8bb54534
YourtionGuo <yourtion@gmail.com>
2017-05-19 18:17:29 +0800

    add C14-Compress Example lz77

--- Example/compress/main.c
+++ Example/compress/main.c
@@ -94,11 +94,70 @@ int main(int argc, char **argv)
     }
   }
 
-  fprintf(stdout, "Data was restored OK\n");
+  fprintf(stdout, "Data was restored OK\n\n");
 
   free(compressed);
   free(restored);
+
+  /// 使用 LZ77 压缩数据
+
+  fprintf(stdout, "Compressing with LZ77\n");
+  fprintf(stdout, "Compressing...");
+
+  if ((csize = lz77_compress(original, &compressed, osize)) < 0) return 1;
+
+  fprintf(stdout, "Done\n");
+  fprintf(stdout, "Uncompressing...");
+
+  if ((rsize = lz77_uncompress(compressed, &restored)) < 0) {
+
+    fprintf(stdout, "\n");
+    free(compressed);
+    return 1;
+  }
+
+  fprintf(stdout, "Done\n");
+  fprintf(stdout, "-> osize=%d, csize=%d, rsize=%d\n", osize, csize, rsize);
+
+  if (rsize != osize) {
+
+    fprintf(stdout, "Data was not properly restored\n");
+    free(compressed);
+    free(restored);
+    return 1;
+
+  } else {
+
+    for (i = 0; i < rsize; i++) {
+
+      if (original[i] != restored[i]) {
+
+        fprintf(stdout, "Data was not properly restored\n");
+
+        if (isgraph(original[i])) {
+          fprintf(stdout, "-> original[%d]=\"%c\"\n", i, original[i]);
+        } else {
+          fprintf(stdout, "-> original[%d]=0x%02x\n", i, original[i]);
+        }
+
+        if (isgraph(restored[i])) {
+          fprintf(stdout, "-> restored[%d]=\"%c\"\n", i, restored[i]);
+        } else {
+          fprintf(stdout, "-> restored[%d]=0x%02x\n", i, restored[i]);
+        }
+
+        free(compressed);
+        free(restored);
+        return 1;
+        
+      }
+    }
+  }
   
+  fprintf(stdout, "Data was restored OK\n\n");
+  
+  free(compressed);
+  free(restored);
+
   return 0;
-  
 }
